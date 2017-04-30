e102e90ce48c2884aa285075168abc6ef7a24464
tanida_saki <a08012220873@gmail.com>
2017-05-01 00:07:34 +0900

    happou

---
+++ Assets/effect.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class effect : MonoBehaviour {
+	public ParticleSystem sparkle;
+	public ParticleSystem sparkle2;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			RaycastHit hit = new RaycastHit ();
+
+			if (Physics.Raycast (ray, out hit)) {
+				sparkle.Emit(1);
+				sparkle2.transform.position = hit.point;
+				sparkle2.Emit (1);
+			}
+		}
+	}
+}
