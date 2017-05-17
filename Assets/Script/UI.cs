2392d2f0f14f98cea8f58b905ba465f3e38cd637
tanida_saki <a08012220873@gmail.com>
2017-05-17 16:22:05 +0900

    Create a timer

---
+++ Assets/Script/UI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI : MonoBehaviour {
+	[SerializeField]private Text timer;
+	float time=90.0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		time -= Time.deltaTime;
+		if (time < 0) {
+			time = 0f;
+		}
+		timer.text = "Time:" + time.ToString ("F1") + "s";
+	}
+}
